d87d8d19bc136957d91bbfd5529d07f7cba31fd6
arcstudionews <arcstudioecos@gmail.com>
2024-06-23 15:43:04 +0700

    Update login.js

--- login.js
+++ login.js
@@ -40,3 +40,44 @@ function statusChangeCallback(response) {  // Called with the results from FB.ge
     });
   }
 
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{1864937180647581}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{v20.0}'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+   FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+
+{
+    status: 'connected',
+    authResponse: {
+        accessToken: '...',
+        expiresIn:'...',
+        signedRequest:'...',
+        userID:'...'
+    }
+}
+
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
